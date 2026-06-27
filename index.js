518e347b78eb924090eda241716412be843f7a5a
Jerry <jerrykharbuki83@gmail.com>
2026-06-27 08:52:41 +0530

    close dialog after form submission

--- index.js
+++ index.js
@@ -6,25 +6,62 @@ document.getElementById("addBook").addEventListener("click"
         dialog.showModal();
 
     }
+
 );
+
+const form = document.getElementById("bookForm");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const book = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+
+    console.log(book);
+
+    dialog.close();
+
+}
+);
+//to close the dialog 
 const close = document.getElementById("closeBtn");
 document.getElementById("closeBtn").addEventListener("click", () => {
     dialog.close();
 }
 );
-const form = document.getElementById(bookForm);
-const SubBtn = document.getElementById(sub);
-const cancelbt = document.getElementById(closeBtn)
+
+//taking the input from user
 
 
 
-//or delete all books 
-const clearBtn = document.querySelector("#clear");
-clearBtn.addEventListener("click", () => {
-    bookarr.length = 0;
-    container.innerHTML = "";
-})
 
-//for delete single book
-container.addEventListener
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//or delete all books
+// const clearBtn = document.querySelector("#clear");
+// clearBtn.addEventListener("click", () => {
+//     bookarr.length = 0;
+//     container.innerHTML = "";
+// })
+
 
